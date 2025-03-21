f3ef2f71f0919ce36f7b3271a6f80063aa4a3b23
chiupam <chiupam@126.com>
2025-03-21 17:00:06 +0800

    将Flask监听地址从127.0.0.1修改为0.0.0.0以支持Docker部署

--- run.py
+++ run.py
@@ -54,4 +54,4 @@ except ImportError:
     app.logger.info('未安装flask_apscheduler，跳过定时任务配置')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5001, debug=True) 
+    app.run(host='0.0.0.0', port=5001, debug=True) 
